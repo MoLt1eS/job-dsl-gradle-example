59fc88038c3fdce4e1439f635eaf4bee3833cca6
molt1es <tjrs02@gmail.com>
2018-07-31 12:19:00 +0100

    Preparing to run example config

--- src/jobs/ciabJobs.groovy
+++ src/jobs/ciabJobs.groovy
@@ -1,11 +1,12 @@
 import com.dslexample.entities.Project
 import hudson.FilePath
+import hudson.model.Executor
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor
 
 
 String basePath = 'CIAB'
-String repo = 'compareeuropegroup/ciab-plugin'
+//String repo = 'compareeuropegroup/ciab-plugin'
 
 folder(basePath) {
     description 'Contains all the CIAB related projects'
@@ -19,13 +20,13 @@ void createCIABJobs(){
     def yaml = new Yaml(constr)
 
     // Build a list of all config files ending in .yml
-    def cwd = hudson.model.Executor.currentExecutor().getCurrentWorkspace().absolutize()
+    def cwd = Executor.currentExecutor().getCurrentWorkspace().absolutize()
     def configFiles = new FilePath(cwd, 'configs').list('*.yml')
 
     // Create/update a pull request job for each config file
     configFiles.each { file ->
         Project projectConfig = yaml.loadAs(file.readToString(), Project.class)
-        def project = projectConfig.project.replaceAll(' ', '-')
+//        def project = projectConfig.project.replaceAll(' ', '-')
 
         job(projectConfig.name) {
             scm {
--- src/main/groovy/com/dslexample/entities/Project.groovy
+++ src/main/groovy/com/dslexample/entities/Project.groovy
@@ -2,9 +2,10 @@ package com.dslexample.entities
 
 /**
  * Created by tiago on 27-07-2018.
+ * Entity example
  */
 class Project {
 
-    String name;
+    String name
 
 }

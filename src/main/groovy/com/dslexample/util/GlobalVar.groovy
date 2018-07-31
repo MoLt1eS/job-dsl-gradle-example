b04ea0acdb6b929ba3cf633bae2cfb133315bba6
molt1es <tjrs02@gmail.com>
2018-07-31 14:49:14 +0100

    Issues with Java Class, changed to Groovy

---
+++ src/main/groovy/com/dslexample/util/GlobalVar.groovy
@@ -0,0 +1,17 @@
+package com.dslexample.util
+
+/**
+ * Created by tiago on 31-07-2018.
+ * Contains all the Global most used constants
+ *
+ * All paths MUST end with character '/' that rule avoids to have code like "A" + "SEPARATOR" + "B"
+ */
+final class GlobalVar {
+
+    static String CIAB = "CIAB";
+    static String CONFIG_DIR = "src/config/";
+    static String CONFIG_CIAB_DIR = CONFIG_DIR + "CIAB";
+
+    static String GITHUB_REPO_LOCATION_URL = "https://github.com/compareeuropegroup/%s.git";
+
+}
